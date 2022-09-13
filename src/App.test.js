644b754fdba46948185ded106fb20ce2b32e3c66
Your Name <nghilt19411@gmail.com>
2022-09-13 19:42:30 +0700

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+test('Fucking', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
